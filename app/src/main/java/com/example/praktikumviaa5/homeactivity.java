90eb9af3bd3a4e20c1f19c3c8f89e5d26d6f1216
octaviabay <octaviabayuk@gmail.com>
2022-05-30 00:08:42 +0700

    Tugas

---
+++ app/src/main/java/com/example/praktikumviaa5/homeactivity.java
@@ -0,0 +1,14 @@
+package com.example.praktikumviaa5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class homeactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activityhome);
+    }
+}
